ef809964713fa8b69cea60347c195f4fe4ee5151
avan <100318118+avansear@users.noreply.github.com>
2025-12-06 23:42:34 -0500

    normalized works :D

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from movies_to_books.fetch_movies import fetch_movies
 from movies_to_books.clean_movies import clean_movies
+from movies_to_books.normalize_title import normalize_title
 import json
 
 raw = fetch_movies(page=1)
 cleaned = clean_movies(raw)
+normalized = normalize_title(cleaned)
 
-print(json.dumps(cleaned, indent=4))
+print(json.dumps(normalized, indent=4))

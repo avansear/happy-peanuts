8aaa63564c536065c1de8a1bcaa691df9ba3a8bf
avan <100318118+avansear@users.noreply.github.com>
2025-12-07 18:04:20 -0500

    pivoting to a new idea, fetching games and cleaning data from RAWG

---
+++ collecting_data/clean_games.py
@@ -0,0 +1,22 @@
+def clean_games(raw_games):
+    """
+    Desc: Cleaning up the raw JSON data, and giving a dictionary that contains name, year, rating, and rank for each game.
+    """
+    output = []
+    
+    for i, game in enumerate(raw_games):
+        title = game["name"]
+        date = game["released"]
+        rating = game["rating"]
+        
+        # Extract year from date
+        game_year = int(date[:4])
+        
+        output.append({
+            "game_name": title,
+            "year": game_year,
+            "rating": rating,
+            "popularity_rank": i + 1
+        })
+    
+    return output
---
+++ collecting_data/fetch_games.py
@@ -0,0 +1,16 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+RAWG_KEY = os.getenv("RAWG_KEY")
+
+def fetch_games_by_year(year):
+    """
+    Desc: Fetching top 5 games for given year from RAWG, in raw form.
+    """
+    url = f"https://api.rawg.io/api/games?key={RAWG_KEY}&dates={year}-01-01,{year}-12-31&ordering=-ratings_count&page_size=5"
+    response = requests.get(url)
+    data = response.json()
+
+    return data["results"]
